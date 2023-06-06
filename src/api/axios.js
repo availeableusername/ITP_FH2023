52d13bc1b151aa9d9b98c01e83df8d4162a8ab49
johannes <127777065+availeableusername@users.noreply.github.com>
2023-06-06 15:47:25 +0200

    Add files via upload

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3000'  //URL ändern auf Backend
+    baseURL: 'http://localhost:3500'  //URL ändern auf Backend, geändert von 3000 auf 4000
 })
+
+const getData = () =>{
+    axios.get('http://localhost:3000').then(response => {
+        console.log(response);
+    })
+};
+
+const sendData = () =>{};
+
