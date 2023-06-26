c0e7b2002f25413fe049ecae2fb130ed6bfafe82
Kazem <kaz.gh22@gmail.com>
2023-06-26 22:23:40 +0200

    User Authen. is added!

--- src/context/server.js
+++ src/context/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const mysql = require('mysql');
+const session = require('express-session');
+const store = new session.MemoryStore();
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(session({
+  secret : 'pass',
+  cookie: { maxAge: 30000},
+  saveUninitialized: false,
+  store
+}))
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -53,6 +62,31 @@ app.post('/register', (req, res) => {
 
 app.get('/', (req, res) => {
   return res.json('From Backend Side');
+});
+
+app.post("/login", (req, res) =>{
+  console.log("The client tries to log in!");
+  console.log(req.sessionID);
+  const {username, password} = req.body;
+  if(username & password){
+    if(req.session.authenticated){
+      res.json(req.session);
+    }else{
+      if(password == '123'){ // get the pass from the database
+        res.session.authenticated = true;
+        res.session. user = {
+          username, password
+        };
+        res.json(req.session);
+      }else{
+        res.status(403).json({ msg: "Bad Credentials"});
+      }
+    }
+  }else{
+    res.status(403).json({ msg: "Bad Credentials"});
+
+  }
+  res.send(200);
 });
 
 app.listen(3050, () => {
