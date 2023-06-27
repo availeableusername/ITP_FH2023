d5c327dbb67d5bbebd6c9fcd5b38f6390e3c340e
johannes <127777065+availeableusername@users.noreply.github.com>
2023-06-27 21:50:16 +0200

    Add files via upload

--- src/components/lorem.js
+++ src/components/lorem.js
@@ -1,1 +1,1 @@
-export const pg1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et neque lectus. Cras non odio eu ipsum placerat egestas. Proin volutpat congue nulla eget venenatis.'
+export const pg1 = 'Taste our great Chicken. Order one of our four tasty menus'
